46223b6b7c2769de9928da70b32542722d9b7974
nukaterrapin7 <pythonism007@gmail.com>
2022-10-12 18:29:54 -0500

    Adds some styling

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get('/oauth2callback', passport.authenticate('google', {
 router.get('/logout', function(req, res, next) {
   req.logout(function(err) {
     if(err) return next(err)
-    res.redirect('/recipes')
+    res.redirect('/')
   });
 });
 
