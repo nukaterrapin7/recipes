f10a461464868a3097246b6e67dea9e31f276dbe
nukaterrapin7 <pythonism007@gmail.com>
2022-10-11 19:01:39 -0500

    Adds comment section, and adjusts some CSS

--- models/recipe.js
+++ models/recipe.js
@@ -2,10 +2,19 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    content: String,
+    rating: {type: String, min: 1, max: 5, default: 5},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true },
+    userName: String,
+    userAvatar: String
+});
+
 const recipeSchema = new Schema({
     name: {type: String, requires: true},
     ingredients: [],
-    directions: []
+    directions: [],
+    comments: [commentSchema]
 }, {
     timestamps: true
 });
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var recipesRouter = require('./routes/recipes');
+const commentsRouter = require('./routes/comments');
 
 var app = express();
 
@@ -40,6 +41,7 @@ app.use(function(req, res, next) {
 
 app.use('/', indexRouter);
 app.use('/recipes', recipesRouter);
+app.use('/', commentsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
